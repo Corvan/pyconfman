6ac4fca4b47e2c671a6db4573a6e9b5c72443aa8
Lars Liedtke <lars@familie-liedtke.net>
2022-05-29 01:55:30 +0200

    add empty lines before and after yields

--- test/fixtures_copy.py
+++ test/fixtures_copy.py
@@ -13,7 +13,9 @@ def prefix():
     prefix_path = pathlib.Path(PREFIX)
     if not prefix_path.exists():
         prefix_path.mkdir()
+
     yield prefix_path
+
     if prefix_path.exists():
         shutil.rmtree(prefix_path)
 
@@ -22,7 +24,9 @@ def prefix():
 def source_file(prefix):
     source_path = pathlib.Path(SOURCE_PATH)
     source_path.write_text("test")
+
     yield source_path
+
     if source_path.exists():
         source_path.unlink()
 
@@ -33,7 +37,9 @@ def source_directory(prefix):
     source_path.mkdir()
     source_file = source_path / source_path.name
     source_file.write_text("test")
+
     yield source_path
+
     for children in source_path.iterdir():
         children.unlink()
     source_path.rmdir()
@@ -43,7 +49,9 @@ def source_directory(prefix):
 def destination_file(prefix):
     destination_path = pathlib.Path(DESTINATION_PATH)
     destination_path.write_text("test")
+
     yield destination_path
+
     destination_path.unlink()
 
 
@@ -51,7 +59,9 @@ def destination_file(prefix):
 def destination_directory(prefix):
     destination_path = pathlib.Path(DESTINATION_PATH)
     destination_path.mkdir()
+
     yield destination_path
+
     if destination_path.is_dir():
         for child in destination_path.iterdir():
             child.unlink()
