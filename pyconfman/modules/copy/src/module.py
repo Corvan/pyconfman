4708c37e6843182be56df9da86363297d3b6e148
Lars Liedtke <lars@familie-liedtke.net>
2022-05-30 20:18:31 +0200

    update module function docstring

--- pyconfman/modules/copy/src/module.py
+++ pyconfman/modules/copy/src/module.py
@@ -13,14 +13,25 @@ def local_copy(
     """
     copy a resource (file or directory)
 
+    this function triggers to copy a resource from a source path to a destination path.
+    It only works with regular files and directories.
 
+    If source and destination are passed as strings the difference between referring to
+    a file or a directory are trailing slashes. A file does not have a trailing slash;
+    a directory has got one
+
+    E.g.:
+
+    - "/tmp/file"
+    - "/tmp/directory/"
 
     :parameter source: the source (file or directory) to copy from
     :parameter destination: the destination (file or directory) to copy to
     :parameter create: create the destination if it does not exist
     :parameter overwrite: overwrite the destination if it exists
     :raises SourceDoesNotExistError: when source does not exist
-    :raises SourceMustBeDirectoryOrFileError: when source is not a file or directory
+    :raises SourceMustBeDirectoryOrFileError: when source is not a regular file or
+            directory
     :raises DestinationExistsError: when destination exists and overwrite is False
     :raises DestinationDoesNotExistError: when create is False and destination does not
             exist
