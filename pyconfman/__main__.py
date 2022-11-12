23eda483cc13b364f195918ed4a4431b845af7ba
Lars Liedtke <lars@familie-liedtke.net>
2022-11-12 17:14:28 +0100

    add __main__.py to create pyconfman executable

--- pyconfman/__main__.py
+++ pyconfman/__main__.py
@@ -1,6 +1,20 @@
-import pyconfman.inventory
+from pathlib import Path
+from pyconfman.inventory import Inventory
 import pyconfman.playbook
 
+try:
+    import tomllib as toml
+except (ImportError, ModuleNotFoundError):
+    import tomli as toml
+
+config: dict
+config_path = Path("config.toml")
+
 if __name__ == "__main__":
-    inventory = pyconfman.inventory.import_module("inventory")
-    pyconfman.playbook.run("playbook", inventory)
+    if config_path.exists():
+        with open("config.toml", "rb") as fd:
+            config = toml.load(fd)["pyconfman"]["main"]
+    config.setdefault("inventory", "inventory")
+    config.setdefault("playbook", "playbook")
+    inventory = Inventory.import_module(config["inventory"])
+    pyconfman.playbook.run(config["playbook"], inventory)
